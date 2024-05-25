abd7f1f8c85738387d354115a6120eab9a1a888d
MahdiT54 <mahditanzim2@gmail.com>
2024-05-25 13:01:30 -0400

    small screen nav menu

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
 document.querySelectorAll(".accordion-item").forEach((item) => {
-    item.addEventListener("click", () => {
-      const button = item.querySelector(".accordion-header__outer");
-      const chevron = button.querySelector(".fa-chevron-up");
-      const accordionContent = button.nextElementSibling;
-      const currentlyActive = document.querySelector(".accordion-header__outer.active");
-  
-      if (currentlyActive && currentlyActive !== button) {
-        currentlyActive.classList.remove("active");
-        currentlyActive.nextElementSibling.style.maxHeight = "0";
-        currentlyActive.querySelector(".fa-chevron-up").classList.remove("up");
-      }
-  
-      button.classList.toggle("active");
-      chevron.classList.toggle("up");
-  
-      if (button.classList.contains("active")) {
-        accordionContent.style.maxHeight = "1000px";
-      } else {
-        accordionContent.style.maxHeight = "0";
-      }
-    });
+  item.addEventListener("click", () => {
+    const button = item.querySelector(".accordion-header__outer");
+    const chevron = button.querySelector(".fa-chevron-up");
+    const accordionContent = button.nextElementSibling;
+    const currentlyActive = document.querySelector(
+      ".accordion-header__outer.active"
+    );
+
+    if (currentlyActive && currentlyActive !== button) {
+      currentlyActive.classList.remove("active");
+      currentlyActive.nextElementSibling.style.maxHeight = "0";
+      currentlyActive.querySelector(".fa-chevron-up").classList.remove("up");
+    }
+
+    button.classList.toggle("active");
+    chevron.classList.toggle("up");
+
+    if (button.classList.contains("active")) {
+      accordionContent.style.maxHeight = "1000px";
+    } else {
+      accordionContent.style.maxHeight = "0";
+    }
   });
+});
+
+function openMenu() {
+  document.body.classList += " menu--open";
+}
+
+function closeMenu() {
+  document.body.classList.remove("menu--open");
+}
